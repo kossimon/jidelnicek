19e092f5f06249de8b3497fc59ea2bc52f7726dd
kossimon <simon@svetlodat.eu>
2023-11-11 16:33:03 +0100

    simplificatioon

--- planner.py
+++ planner.py
@@ -1,101 +1,78 @@
 import streamlit as st
 import pandas as pd
-import random
 
-# Load the data
-meals_df = pd.read_csv('static/csv/recipe-kcal.csv')
-recipes_df = pd.read_csv('static/csv/recipe-ingredient.csv')
-recipes_df["alternative_amount"].fillna("N/A", inplace=True)
-recipes_df["alternative_unit"].fillna("N/A", inplace=True)
+# Load the provided CSV files to inspect their structure
+meals_df = pd.read_csv('/mnt/data/recipe-kcal.csv')
+recipes_df = pd.read_csv('/mnt/data/recipe-ingredient.csv')
 
-def main():
+# Display the first few rows of each dataframe to understand their structure
+meals_df_head = meals_df.head()
+recipes_df_head = recipes_df.head()
+
+meals_df_head, recipes_df_head
+
+# Function to calculate the total portions for a meal
+def calculate_total_portions(days, eaters):
+    # Portion sizes per person per day
+    portions = {'Simon': 1, 'Maiia': 0.7, 'Both': 1.7}
+
+    # Calculate total portions for the meal
+    total_portions = sum([portions[eater] for eater in eaters]) * len(days)
+    return total_portions
+
+# Function to generate the shopping list
+def generate_shopping_list(selected_meals, total_portions, recipes_df):
+    shopping_list = {}
+
+    # Iterate over each selected meal
+    for meal in selected_meals:
+        # Filter the recipes_df for the current meal
+        meal_ingredients = recipes_df[recipes_df['recipe_name'] == meal]
+
+        # Calculate the required amount for each ingredient
+        for _, row in meal_ingredients.iterrows():
+            ingredient = row['ingredient']
+            amount = row['unit_amount'] * total_portions
+
+            # Add to the shopping list
+            if ingredient in shopping_list:
+                shopping_list[ingredient] += amount
+            else:
+                shopping_list[ingredient] = amount
+
+    # Convert the shopping list to a DataFrame
+    shopping_list_df = pd.DataFrame(list(shopping_list.items()), columns=['Ingredient', 'Amount'])
+    return shopping_list_df
+
+# Streamlit App
+def meal_planner_app():
     st.title("Meal Planner")
 
-    # User selects the days they want to cook
-    st.subheader("Select the days you want to cook:")
+    # Step 1: Day Selection
+    st.subheader("Select the days for which you want to cook:")
     days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+    selected_days = st.multiselect("Days", days)
 
-    # Display checkboxes for each day in a single row
-    day_columns = st.columns(len(days))
-    selected_days = [day_columns[i].checkbox(day, value=False, key=day) for i, day in enumerate(days)]
-
-    # Setup for meal time and meal preferences
+    # Step 2: Meal Selection
+    st.subheader("Select Meals:")
     meal_times = ["breakfast", "lunch", "snack", "I. dinner", "II. dinner"]
-    meal_preferences = ["Maiia", "Simon", "Both"]
+    selected_meals = {meal_time: st.selectbox(f"{meal_time.capitalize()}", meals_df['recipe_name'].unique()) 
+                      for meal_time in meal_times}
 
-    # Loop through each day to set up meal selections
-    for i, day in enumerate(days):
-        if selected_days[i]:
-            st.subheader(f"Meals to cook on {day}")
-            columns = st.columns(len(meal_times))
+    # Step 3: People Selection
+    st.subheader("Select who will be eating:")
+    eaters_options = ["Simon", "Maiia", "Both"]
+    selected_eaters = {meal_time: st.selectbox(f"Eaters for {meal_time.capitalize()}", eaters_options)
+                       for meal_time in meal_times}
 
-            for j, meal_time in enumerate(meal_times):
-                with columns[j]:
-                    # Meal options based on time of the day
-                    meal_options = ['None'] + meals_df[meals_df['meal'].str.lower() == meal_time.lower()]['recipe_name'].tolist()
-                    
-                    # Selection of the meal
-                    default_key = f"default_{day}_{meal_time}"
-                    if default_key not in st.session_state:
-                        st.session_state[default_key] = random.choice(meal_options[1:])
-                    
-                    default_option = st.session_state[default_key]
-                    selected_meal = st.selectbox(f"{meal_time}", meal_options, index=meal_options.index(default_option), key=f"{day}_{meal_time}")
+    # Step 4: Calculate Portions and Generate Shopping List
+    if st.button("Generate Shopping List"):
+        shopping_list = pd.DataFrame()
+        for meal_time, meal in selected_meals.items():
+            total_portions = calculate_total_portions(selected_days, [selected_eaters[meal_time]])
+            meal_shopping_list = generate_shopping_list([meal], total_portions, recipes_df)
+            shopping_list = pd.concat([shopping_list, meal_shopping_list])
 
-                    # Selection for who will eat the meal
-                    preference_key = f"preference_{day}_{meal_time}"
-                    if preference_key not in st.session_state:
-                        st.session_state[preference_key] = "Both"
-                    st.radio("Who will eat?", meal_preferences, key=preference_key)
-
-    # Process to create a shopping list based on selected meals
-    if st.button("Make Shopping List"):
-        chosen_recipes = {}
-
-        # Determine portions needed for each selected day
-        for i, day in enumerate(days):
-            if selected_days[i]:
-                portions = 1  # Assign one portion per selected day
-
-                for meal_time in meal_times:
-                    chosen_recipe = st.session_state.get(f"{day}_{meal_time}")
-                    preference = st.session_state.get(f"preference_{day}_{meal_time}", "Both")
-                    eaters = 2 if preference == "Both" else 1
-                    if chosen_recipe and chosen_recipe != 'None':
-                        chosen_recipes.setdefault(chosen_recipe, 0)
-                        chosen_recipes[chosen_recipe] += portions * eaters
-
-        # Display chosen recipes and calculate ingredients needed
-        for recipe, portions in chosen_recipes.items():
-            st.write(f"{recipe}: {portions} portions")
-
-            recipe_ingredients = recipes_df[recipes_df['recipe_name'] == recipe]
-            data = []
-            for _, row in recipe_ingredients.iterrows():
-                ingredient = row['ingredient']
-                amount = round(row['unit_amount'] * portions, 2)
-                data.append([ingredient, f'{amount} {row["unit"]}'])
-
-            df = pd.DataFrame(data, columns=['Ingredient', 'Amount'])
-            st.table(df)
-
-        # Generate the shopping list
-        shopping_list = {}
-        for recipe, portions in chosen_recipes.items():
-            recipe_ingredients = recipes_df[recipes_df['recipe_name'] == recipe]
-            for _, row in recipe_ingredients.iterrows():
-                ingredient = row['ingredient']
-                amount = round(row['unit_amount'] * portions, 2)
-                shopping_list.setdefault(ingredient, 0)
-                shopping_list[ingredient] += amount
-
-        st.subheader("Shopping List")
-        data = []
-        for ingredient, amount in shopping_list.items():
-            data.append([ingredient, f'{round(amount, 2)} {recipes_df[recipes_df["ingredient"] == ingredient]["unit"].iloc[0]}'])
-
-        df = pd.DataFrame(data, columns=['Ingredient', 'Total Amount'])
-        st.table(df)
-
-if __name__ == "__main__":
-    main()
+        # Display the shopping list
+        st.subheader("Shopping List:")
+        st.write(shopping_list.groupby('Ingredient').sum().reset_index())
